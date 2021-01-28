6c6111e110ba8d01b9bda0e99b25c1ed08443037
Carolina Ponce <caromponce@gmail.com>
2021-01-28 20:32:49 -0300

    arrays

--- script.js
+++ script.js
@@ -6,15 +6,21 @@ class Producto {
         this.stock = stockParam;
     }
     sumarPrecio() {
-        carrito = carrito + this.precio;
+        carritoPrecio = carritoPrecio + this.precio;
     }
 }
 
 let producto1 = new Producto('Short con calza', 'res/shortConCalza2.jpg', 2200, 5);
+let producto2 = new Producto('Top deportivo turqueza', 'res/topDeportivoTurquesa.jpg', 1200, 6);
+let producto3 = new Producto('Short calza', 'res/shortCalza.jpg', 1400, 8);
 
-var carrito = 0;
+var carritoPrecio = 0;
 
-function sumarAlCarrito() {
-    producto1.sumarPrecio();
-    console.log(`La suma de su carrito es: ${carrito}`);
+var productos = [producto1, producto2, producto3];
+var objetosCarritos = [];
+
+function sumarAlCarrito(indice) {
+    productos[indice].sumarPrecio();
+    objetosCarritos.push(productos[indice]);
+    console.log(`La suma de su carrito es: ${carritoPrecio}`);
 }
