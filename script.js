55d5d6b387c4e3b8ba6731f47c6eede0437c15dc
Carolina Ponce <caromponce@gmail.com>
2021-03-02 21:21:09 -0300

    efecto y animaciones

--- script.js
+++ script.js
@@ -20,17 +20,32 @@ window.addEventListener('load', function () {
         productos.push(producto);
         cargarTarjetas(producto, indiceBDD);
     }
-    // var containerTarjetas = document.getElementById('containerTarjetas');
-    // containerTarjetas.innerHTML = tarjetaHTML;
     $("#containerTarjetas").html(tarjetaHTML); //jquery
 
-
-
+    $('.btnOcultoCard').hide();
     $('.card--inicio a').on('click', function () {
         let indiceProducto = this.id.split('_')[1];
         sumarAlCarrito(indiceProducto);
+        animarCarrito();
+        switchBtnAgregarCarrito(this);
     })
 });
+
+function animarCarrito() {
+    $('#badgeCarritoForma').animate({
+        fontSize: '1.85em'
+    }, "slow", function () {
+        $('#badgeCarritoForma').animate({
+            fontSize: '1.33333em'
+        }, "slow")
+    })
+}
+
+function switchBtnAgregarCarrito(btnPress) {
+    $(btnPress).fadeOut("fast", function () {
+        $(btnPress).siblings("a").fadeIn("slow")
+    })
+}
 
 function sumarAlCarrito(indice) {
     productos[indice].sumarPrecio();
@@ -42,11 +57,33 @@ function sumarAlCarrito(indice) {
             <div class="col-6"> 
                 <p>${objetosCarritos[objetosCarritos.length - 1].nombre}</p>
             </div>
-            <div class="col-6">
+            <div class="col-3 seleccionStock">
+                <div class="qty">
+                    <span class="minus bg-dark">-</span>
+                    <input type="number" class="count" name="qty" value="1" disabled>
+                    <span class="plus bg-dark">+</span>
+                </div>
+            </div>
+            <div class="col-3">
                 <p>$${objetosCarritos[objetosCarritos.length - 1].precio}</p>
             </div>
         </div>`); //jquery
     $("#sumaModalCarrito").html(`La suma de su carrito es: ${carritoPrecio}`); //jquery
+    verificarStock();
+}
+
+function verificarStock() {
+    $(".plus").on('click', function () {
+        let inputBrother = $(this).siblings('input');
+        inputBrother.val(parseInt(inputBrother.val()) + 1);
+    });
+    $(".minus").on('click', function () {
+        let inputBrother = $(this).siblings('input');
+        inputBrother.val(parseInt(inputBrother.val()) - 1);
+        if (inputBrother.val() == 0) {
+            inputBrother.val(1);
+        }
+    });
 }
 
 function cargarTarjetas(productoParaCargarHTML, indiceBDD) {
@@ -64,7 +101,8 @@ function cargarTarjetas(productoParaCargarHTML, indiceBDD) {
     <div class="card-body">
     <h5 class="card-title">${productoParaCargarHTML.nombre}</h5>
     <p class="card-text">$ ${productoParaCargarHTML.precio}</p>
-    <a href="#" class="btn btn-primary ${btnDeshabilitado}" id="producto_${indiceBDD}">${disponibilidadStock}</a>
+    <a  class="btn btn-primary ${btnDeshabilitado}" id="producto_${indiceBDD}">${disponibilidadStock}</a>
+    <a  class="btn btn-primary disabled btnOcultoCard"> Agregado al Carrito</a>
     </div>
     </div>
     `
