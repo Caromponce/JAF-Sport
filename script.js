c8ec83ed3d0dd9ade6fc52b7fd984057aaca1aab
Carolina Ponce <caromponce@gmail.com>
2021-03-21 02:12:22 -0300

    Modal, eliminar productos, css.

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 var productos = [];
-var productosBDD = [];
-var carritoPrecio = 0;
-var objetosCarritos = [];
+var carrito = localStorage.setItem('carrito', JSON.stringify([]));
 var tarjetaHTML = '';
 class Producto {
     constructor(idParam, nombreParam, urlImagenParam, precioParam, stockParam) {
@@ -11,12 +9,63 @@ class Producto {
         this.precio = precioParam;
         this.stock = stockParam;
     }
-    sumarPrecio() {
-        carritoPrecio = carritoPrecio + this.precio;
+}
+
+function sumarObjetoAlCarrito(idParam, nombreParam, precioParam, cantidadParam) {
+    let carrito = JSON.parse(localStorage.getItem('carrito'));
+    let item = {
+        id: idParam,
+        nombre: nombreParam,
+        precio: precioParam,
+        cantidad: cantidadParam,
+        subtotal: precioParam * cantidadParam
+    }
+    carrito.push(item);
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+    // carrito.forEach(element => {
+    //     element.nombre  
+    // });
+    return carrito.length;
+}
+
+function getTotal() {
+    let carrito = JSON.parse(localStorage.getItem('carrito'));
+    let acumuladorTotal = 0;
+    carrito.forEach(item => {
+        acumuladorTotal = acumuladorTotal + item.subtotal;
+    });
+    return acumuladorTotal;
+}
+
+function actualizarSubtotal(idProductoApretado, nuevaCantidad) {
+    let carrito = JSON.parse(localStorage.getItem('carrito'));
+    const indiceItemEncontrado = carrito.findIndex(item => (item.id == idProductoApretado));
+    if (nuevaCantidad === 0) {
+        carrito = carrito.filter(item => item.id != idProductoApretado);
+    } else {
+        carrito[indiceItemEncontrado].cantidad = nuevaCantidad;
+        carrito[indiceItemEncontrado].subtotal = nuevaCantidad * carrito[indiceItemEncontrado].precio;
+    }
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+    if (carrito.length == 0) {
+        $("#modal-bodyCarritoVacio").show();
+        $("#carrito button[type=submit]").hide();
+        $("#sumaModalCarrito").html(``); //jquery
+    } else {
+        $("#sumaModalCarrito").html(`La suma de su carrito es: $${getTotal()}`); //jquery
     }
 }
 
+function eliminarItem(idProductoApretado) {
+    let carrito = JSON.parse(localStorage.getItem('carrito'));
+    const indiceItemEncontrado = carrito.findIndex(item => (item.id == idProductoApretado));
+    carrito.splice(indiceItemEncontrado, 1);
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+    $("#sumaModalCarrito").html(`La suma de su carrito es: $${getTotal()}`); //jquery
+}
 window.addEventListener('load', function () {
+    $('#loader').hide();
+    let productosBDD;
     $.ajax({
         url: "bdd.json",
         async: false,
@@ -34,12 +83,34 @@ window.addEventListener('load', function () {
 
     $('.btnOcultoCard').hide();
     $('.card--inicio a').on('click', function () {
-        let indiceProducto = this.id.split('_')[1];
+        let indiceProducto = $(this).data("idprod");
         sumarAlCarrito(indiceProducto);
         animarCarrito();
         switchBtnAgregarCarrito(this);
     })
 });
+
+function cargarTarjetas(productoParaCargarHTML, indiceBDD) {
+    let disponibilidadStock = '';
+    let btnDeshabilitado = '';
+    if (productoParaCargarHTML.stock > 0) {
+        disponibilidadStock = 'Agregar al carrito';
+    } else {
+        disponibilidadStock = 'No hay stock';
+        btnDeshabilitado = 'disabled';
+    }
+    tarjetaHTML = tarjetaHTML +
+        `<div class="flex-fill card card--inicio mx-4 my-2 p-2" style="width: 18rem;">
+    <img src="${productoParaCargarHTML.urlImagen}" class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">${productoParaCargarHTML.nombre}</h5>
+    <p class="card-text">$ ${productoParaCargarHTML.precio}</p>
+    <a  class="btn btn-primary ${btnDeshabilitado}" data-idprod="${indiceBDD}">${disponibilidadStock}</a>
+    <a  class="btn btn-primary disabled btnOcultoCard"> Agregado al Carrito</a>
+    </div>
+    </div>
+    `
+}
 
 function animarCarrito() {
     $('#badgeCarritoForma').animate({
@@ -57,36 +128,44 @@ function switchBtnAgregarCarrito(btnPress) {
     })
 }
 
-function sumarAlCarrito(indice) {
-    productos[indice].sumarPrecio();
-    objetosCarritos.push(productos[indice]);
-    $("#badgeCarrito").html(objetosCarritos.length); //jquery
+function sumarAlCarrito(idProd) {
+    let precioProductoSeleccionado = productos[idProd].precio;
+    let nombreProductoSeleccionado = productos[idProd].nombre;
+    let cantidadItems = sumarObjetoAlCarrito(idProd, nombreProductoSeleccionado, precioProductoSeleccionado, 1);
+    $("#badgeCarrito").html(cantidadItems); //jquery
     let textoOld = $("#carrito .modal-bodyProductos").html();
     $("#carrito .modal-bodyProductos").html(textoOld +
-        `<div class="row">
-            <div class="col-6"> 
-                <p>${objetosCarritos[objetosCarritos.length - 1].nombre}</p>
+        `<div class="row mx-2 mb-2">
+            <div class="col-4"> 
+                <p>${nombreProductoSeleccionado}</p>
             </div>
-            <div class="col-3 seleccionStock">
+            <div class="col-3 p-0 seleccionStock">
                 <div class="qty">
-                    <span class="minus bg-dark">-</span>
-                    <input type="number" class="count" name="qty" value="1" disabled>
-                    <span class="plus bg-dark" data-idprod=${indice}>+</span>
+                    <button class='minuss rounded-pill btn-outline-dark btn p-1'><i class="fa fa-minus"></i></button>
+                    <input type="number" class="count" name="qty" value="1" data-idprod=${idProd} disabled>
+                    <button class='pluss rounded-pill btn-outline-dark btn p-1'><i class="fa fa-plus"></i></button>
                 </div>
             </div>
+            <div class="col-1">
+                <span class="font-weight-bold">x</span>
+            </div>
             <div class="col-3">
-                <p>$${objetosCarritos[objetosCarritos.length - 1].precio}</p>
+                <p>$${precioProductoSeleccionado}</p>
+            </div>
+            <div class="col-1">
+                <button class='trashh rounded-pill btn-outline-dark btn p-1' data-idprod=${idProd}>
+                    <i class="fa fa-trash"></i>
+                </button>
             </div>
         </div>`); //jquery
-    $("#sumaModalCarrito").html(`La suma de su carrito es: ${carritoPrecio}`); //jquery
-    verificarStock();
+    $("#sumaModalCarrito").html(`La suma de su carrito es: $${getTotal()}`); //jquery
+    loadListeners();
 }
 
-function verificarStock() {
-    $(".plus").on('click', function () {
+function loadListeners() {
+    $(".pluss").on('click', function () {
         let inputBrother = $(this).siblings('input');
-        let idProductoApretado = $(this).data("idprod");
-        console.log(idProductoApretado);
+        let idProductoApretado = $(inputBrother).data("idprod");
         $.ajax({
             url: "bdd.json",
             dataType: "json",
@@ -94,40 +173,64 @@ function verificarStock() {
                 productos.forEach(element => {
                     if (element.id == idProductoApretado) {
                         if ((inputBrother.val()) < element.stock) {
-                            inputBrother.val(parseInt(inputBrother.val()) + 1);
+                            let suma = parseInt(inputBrother.val()) + 1;
+                            inputBrother.val(suma).attr('value', suma);
+                            actualizarSubtotal(idProductoApretado, suma);
                         }
                     }
                 });
             }
         })
     });
-    $(".minus").on('click', function () {
+    $(".minuss").on('click', function () {
         let inputBrother = $(this).siblings('input');
-        inputBrother.val(parseInt(inputBrother.val()) - 1);
+        let idProductoApretado = $(inputBrother).data("idprod");
+        let resta = parseInt(inputBrother.val()) - 1;
+        inputBrother.val(resta).attr('value', resta);
         if (inputBrother.val() == 0) {
-            inputBrother.val(1);
+            inputBrother.val(1).attr('value', parseInt(1));
+        } else {
+            actualizarSubtotal(idProductoApretado, resta);
         }
+    });
+    $(".trashh").on('click', function () {
+        let idProductoApretado = $(this).data("idprod");
+        actualizarSubtotal(idProductoApretado, 0);
+        $(this).parents('.row').remove();
+        $('a[data-idprod=' + idProductoApretado + ']').siblings("a").fadeOut("fast", function () {
+            $(this).siblings("a").fadeIn("slow")
+        })
     });
 }
 
-function cargarTarjetas(productoParaCargarHTML, indiceBDD) {
-    let disponibilidadStock = '';
-    let btnDeshabilitado = '';
-    if (productoParaCargarHTML.stock > 0) {
-        disponibilidadStock = 'Agregar al carrito';
-    } else {
-        disponibilidadStock = 'No hay stock';
-        btnDeshabilitado = 'disabled';
-    }
-    tarjetaHTML = tarjetaHTML +
-        `<div class="card card--inicio" style="width: 18rem;">
-    <img src="${productoParaCargarHTML.urlImagen}" class="card-img-top" alt="...">
-    <div class="card-body">
-    <h5 class="card-title">${productoParaCargarHTML.nombre}</h5>
-    <p class="card-text">$ ${productoParaCargarHTML.precio}</p>
-    <a  class="btn btn-primary ${btnDeshabilitado}" id="producto_${indiceBDD}">${disponibilidadStock}</a>
-    <a  class="btn btn-primary disabled btnOcultoCard"> Agregado al Carrito</a>
-    </div>
-    </div>
-    `
-}
+
+//eventos generales
+$(function () {
+    //evento click abrir carrito
+    $('#btnOpenCarrito').on('click', function (event) {
+        let carrito = JSON.parse(localStorage.getItem('carrito'));
+        if (carrito.length == 0) {
+            $("#modal-bodyCarritoVacio").show();
+            $("#carrito button[type=submit]").hide();
+            $("#modal-bodyCarritoComprado").hide();
+        } else {
+            $("#modal-bodyCarritoVacio").hide();
+            $("#carrito button[type=submit]").show();
+            $('#modal-bodyCarritoComprado').hide();
+        }
+    })
+
+    //evento click inicar compra
+    $("#carrito button[type=submit]").on('click', function (event) {
+        $('#loader').show();
+        setTimeout(function (e) {
+            let carrito = JSON.parse(localStorage.getItem('carrito'));
+            //proximamente => enviar datos de carrito al backend...
+            $('.modal-bodyProductos').hide();
+            $('#modal-bodyCarritoComprado').show();
+            $("#sumaModalCarrito").hide(); //jquery
+            $("#carrito button[type=submit]").hide();
+            $('#loader').hide();
+        }, 2000)
+    })
+})
